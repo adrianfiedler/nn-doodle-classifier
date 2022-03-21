a0cc3c8bbb3c6ee8e7dd9d6999819df78da592a6
Adrian <adrian_fiedler@msn.com>
2022-03-21 14:02:20 +0100

    load all data

--- sketch.js
+++ sketch.js
@@ -1,19 +1,20 @@
-let dataTrees;
-let dataBirds;
-let dataApples;
+let trees;
+let birds;
+let apples;
+
 let dataLoaded = 0;
 let done = false;
-let total = 1000;
+// let total = 1000;
 let bytesPerImage = 784;
 let headerBytes = 0;
-let outArray = new Uint8Array(new ArrayBuffer(total*bytesPerImage));
+// let outArray = new Uint8Array(new ArrayBuffer(total*bytesPerImage));
 let imageNo = 0;
 let outByteIndex = 0;
 
 function preload() {
-  dataTrees = loadBytes("data/trees1000.bin", loadingDone);
-  dataBirds = loadBytes("data/birds1000.bin", loadingDone);
-  dataApples = loadBytes("data/apples1000.bin", loadingDone);
+  trees = loadBytes("data/trees1000.bin", loadingDone);
+  birds = loadBytes("data/birds1000.bin", loadingDone);
+  apples = loadBytes("data/apples1000.bin", loadingDone);
 }
 function loadingDone() {
   dataLoaded++;
@@ -22,11 +23,30 @@ function loadingDone() {
 }
 
 function setup() {
-  createCanvas(400, 400);
-  background(255);
+  createCanvas(280, 280);
+  background(0);
+
+  let total = 100;
+  for (let n = 0; n < total; n++) {
+    let img = createImage(28, 28);
+    img.loadPixels();
+    let offset = n * bytesPerImage;
+    for (let i = 0; i < bytesPerImage; i++) {
+      let val = 255 - trees.bytes[i + offset];
+      img.pixels[i * 4 + 0] = val;
+      img.pixels[i * 4 + 1] = val;
+      img.pixels[i * 4 + 2] = val;
+      img.pixels[i * 4 + 3] = 255;
+    }
+    img.updatePixels();
+    let x = (n % 10) * 28;
+    let y = floor(n / 10) * 28;
+    image(img, x, y);
+  }
 }
 
 function draw() {
+  /*
   if (dataLoaded >= 3 && !done) {
     let start = headerBytes;
     let img = createImage(28, 28);
@@ -34,22 +54,23 @@ function draw() {
     let index = start + imageNo * bytesPerImage;
     for (let i = 0; i < img.width; i++) {
       for (let j = 0; j < img.height; j++) {
-        let r = data.bytes[index];
-        let col = color(255 - r, 255 - r, 255 - r);
+        let r = dataTrees.bytes[index];
+        let col = color(r, r, r);
         img.set(j, i, col);
         index++;
-        outArray[outByteIndex++] = r;
+        // outArray[outByteIndex++] = r;
       }
     }
     img.updatePixels();
     image(img, 0, 0, 28, 28);
     console.log(imageNo);
-    if (imageNo++ >= total) {
+    if (imageNo++ >= total - 1) {
       console.log("DONE: passed " + total + " images");
       done = true;
       // downloadBlob();
     }
   }
+  */
 }
 
 function downloadBlob() {
